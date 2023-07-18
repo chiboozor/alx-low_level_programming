09635ec5eaf3ccf9f41da4542ad947c7513f0197
chiboozor <obianochibuzor4@gmail.com>
2023-07-18 13:29:59 +0100

    Print characters using user-defined functions.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - program's entry point.
+ * _putchar - user-defined function for printing characters to stdout.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+				README FOR FUNCTIONS AND NESTED LOOPS.
+
+0-putchar.c - a program that prints '\_putchar' using the user-written function \_putchar. This \_putchar function works the same way as predefined 'putchar' function.
+
+
