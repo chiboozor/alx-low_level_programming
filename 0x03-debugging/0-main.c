d46b919d507431a041e0a7fd7cdaac41f0f514ab
chiboozor <obianochibuzor4@gmail.com>
2023-07-19 14:37:37 +0100

    Tests a function.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - program entry point.
- * Return: 0 on success.
- *
- * positive_or_negative: fxn used to test whether a number is +ve or -ve.
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
