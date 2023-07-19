e0a94cd89fee6c6594b40f1c0440075ea8b3f099
chiboozor <obianochibuzor4@gmail.com>
2023-07-19 14:08:25 +0100

    Files for testing a function.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program entry point.
+ * Return: 0 on success.
+ *
+ * positive_or_negative: fxn used to test whether a number is +ve or -ve.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+				README FOR DEBUGGING TASKS.
+
+Task 1 creates a C source file used to test a function already created and prototyped in the main.h header file. 
+
+
