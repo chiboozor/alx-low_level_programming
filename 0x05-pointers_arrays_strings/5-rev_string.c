6bae76186c35d9fbd371f95da2531dfe0251de8d
chiboozor <obianochibuzor4@gmail.com>
2023-07-26 21:02:08 +0100

    Reverses a string.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,6 @@ void rev_string(char *s)
 	int i = 0; /* Counter variable. */
 	int count = 0; /* Holds the length of the string s. */
 	char b; /* Holds the value of s[i] initially. */
-	char *arr; /* Dynamic declaration of pointer to array. */
 
 	while (s[count] != '\0')
 	{
@@ -22,15 +21,11 @@ void rev_string(char *s)
 		count++;
 	}
 
-	/* Allocates space for array whose size is determined by the string length. */
-	arr = (char *) malloc(count * sizeof(char));
-
-	for (i = 0; i < s[count] / 2; i++)
+	for (i = 0; i < count / 2; i++)
 	{
 		b = s[i];
-		s[i] = s[count];
-		s[count--] = b;
+		s[i] = s[count - i - 1];
+		s[count - i - 1] = b;
 
 	}
-	free(arr);
 }
