289fd8af90d400fcbf9cb49bba45ea0e3f969023
chiboozor <obianochibuzor4@gmail.com>
2023-07-28 09:39:54 +0100

    Header file.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from src into dest.
+ * @src: pointer to source string.
+ * @dest: pointer to destination string.
+ * Return: pointer to destination string.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	/* Loop variable. */
+	int i;
+
+	/* Get the length of src string. */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
