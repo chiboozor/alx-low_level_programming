86c68f735d2b767fe642a98c19e7bdd01f7f7df0
chiboozor <obianochibuzor4@gmail.com>
2023-07-28 12:40:42 +0100

    Concatenates n elements of a string to another.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n elements of the
+ * src string to the dest string.
+ * @src: pointer to the source string.
+ * @dest: pointer to destination string.
+ * @n: number of elements of src string to
+ * append to dest string.
+ * Return: pointer to the destination string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/* Loop variable. */
+	int i;
+
+	/* Length of strings. */
+	int len = 0;
+	int len2 = 0;
+
+	/* Length of src string. */
+	for (i = 0; src[i] != '\0'; i++)
+		len++;
+
+	/* Length of dest string. */
+	for (i = 0; dest[i] != '\0'; i++)
+		len2++;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len2 + i] = src[i];
+	}
+
+	return (dest);
+}
