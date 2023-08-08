280c358443c132fcb93e0d6d32b34e81276e05ea
chiboozor <obianochibuzor4@gmail.com>
2023-08-08 13:44:25 +0100

    Adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - program entry point
@@ -30,14 +31,16 @@ int main(int argc, __attribute__((unused)) char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		/* Checks if the argument passed is not a digit */
-		if (*argv[i] != 0 && *argv[i] != 9)
+		if (!isdigit(*argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		
-		/* Converts the arg into an integer */
-		sum = sum + atoi(argv[i]);
+		else
+		{
+			/* Converts the arg into an integer */
+			sum = sum + atoi(argv[i]);
+		}
 	}
 	printf("%d\n", sum);
 
