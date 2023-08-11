0ffea36e23122651af0417af0dea21bdb481e2f9
chiboozor <obianochibuzor4@gmail.com>
2023-08-11 11:11:06 +0100

    Allocate space in memory for a 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,8 +18,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* Memory allocation */
-	array_ptr = (int **) malloc(sizeof(**array_ptr) * height);
+	/**
+	 * Memory allocation is done by multiplying the size of the array
+	 * pointer in the array of pointers by the height of the array to
+	 * be created in memory.
+	 */
+	array_ptr = malloc(sizeof(*array_ptr) * height);
 
 	if (array_ptr == NULL)
 		return (NULL);
@@ -29,6 +33,15 @@ int **alloc_grid(int width, int height)
 	{
 		/* Memory is allocated for the rows */
 		array_ptr[i] = malloc(sizeof(int) * width);
+
+		/* Frees all memory if **array_ptr is NULL */
+		if (array_ptr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array_ptr[i]);
+			free(array_ptr);
+			return (NULL);
+		}
 		/* Inner loop iterates through the rows of the array */
 		for (j = 0; j < width; j++)
 		{
