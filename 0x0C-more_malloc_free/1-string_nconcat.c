b023a035aef3b73b2d3d350e82ba7498693c4a41
chiboozor <obianochibuzor4@gmail.com>
2023-08-13 17:27:36 +0100

    Dynamically concatenates n elements of destination string to source string and returns pointer to new string in memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, nums, len = 0, len2 = 0;
+	unsigned int i, j, len = 0, len2 = 0;
 	char *str_ptr;
 	
 	/* nums = n; */
