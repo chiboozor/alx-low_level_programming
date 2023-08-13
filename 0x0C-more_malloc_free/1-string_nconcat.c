2243b0daa4ec5b4629091fe5520e6af8c23d1101
chiboozor <obianochibuzor4@gmail.com>
2023-08-13 17:03:40 +0100

    Dynamically concatenates n elements of destination string to source string and returns pointer to new string in memory

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: destination string
+ * @s2: source string
+ * @n: number of bytes of s2 to add to s1
+ * Return: pointer to new s1, NULL on failure
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len = 0, len2 = 0;
+	char *str_ptr;
+
+	/* Treats s1 and s2 as empty strings if NULL is passed */
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	/* Get lengths of s1 and s2 and save in len and len2 respectively */
+	for (i = 0; s1[i] != '\0'; i++)
+		len++;
+	for (j = 0; s2[j] != '\0'; j++)
+		len2++;
+
+	/* Allocates memory for the new string */
+	str_ptr = malloc((len + len2 + 1) * sizeof(char));
+
+	/* Returns NULL if malloc fails */
+	if (str_ptr == NULL)
+		return (NULL);
+
+	/* Returns source string if num of bytes to concat >= len of dest string */
+	if (n >= len2)
+		return (s2);
+
+	/* Concats elements of s1 to new string in memory */
+	for (i = 0; i < len; i++)
+		str_ptr[i] = s1[i];
+
+	/* Concats n elements of s2 to new string from the end of the new string */
+	for (j = 0; j < n; j++)
+		str_ptr[len + j] = s2[j];
+
+	/* Adds terminating null character at the end of the new string */
+	str_ptr[len2 + 1] = '\0';
+
+	return (str_ptr);
+}
