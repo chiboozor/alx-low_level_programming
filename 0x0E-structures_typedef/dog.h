24b093b5bf04e486c11b4838c0a95595269b3bf7
chiboozor <obianochibuzor4@gmail.com>
2023-08-15 17:14:59 +0100

    HEADER FILE

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+
+/**
+ * struct dog - basic information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_STRUCT */
