e3528633ecb1dd0cb53c5fcf93a0b403bb9aeb26
chiboozor <obianochibuzor4@gmail.com>
2023-08-18 12:40:33 +0100

    Prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(param_list, n);
 
+	if (separator == NULL)
+		return;
+
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-		{
-			return;
-		}
 		printf("%d", va_arg(param_list, int));
 		if (i < (n - 1))
 		{
