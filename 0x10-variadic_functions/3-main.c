1415e5f28cdcf557bd9c664983bad393af6943e7
chiboozor <obianochibuzor4@gmail.com>
2023-08-19 13:10:22 +0100

    TEST FILES

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceiss", 'B', 4, NULL, "stSchool");
+    return (0);
+}
